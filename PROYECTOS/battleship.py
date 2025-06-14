211dbb718121dd003f0431433a371d7594a0f181
AnaGu <anagu89@gmail.com>
2025-06-14 19:29:49 +0200

    Update battleship.py

--- PROYECTOS/battleship.py
+++ PROYECTOS/battleship.py
@@ -20,7 +20,8 @@ print("The Bridge, June 2025\n\n\n")
 
 
 # PASO 2: función para crear el tablero
-# si lo quiero de tamaño pongo "lado = 10" (10x10) o como yo quiera de grande en PASO 9
+# si lo quiero de tamaño pongo "lado = 10" (10x10) o como yo quiera
+# en este caso, lo haremos como queramos de grande en el PASO 9
 
 def crea_tablero(lado):                   
     tablero = np.full((lado,lado)," ")
@@ -30,7 +31,7 @@ def crea_tablero(lado):
 
 # PASO 3: reglas para colocar barcos
 
-def coloca_barco_plus(tablero, barco):
+def coloca_barco_plus(tablero, barco, mostrar_proceso):
     tablero_temp = tablero.copy()
     num_max_filas = tablero.shape[0]        # calcula el número de filas que tiene el tablero        
     num_max_columnas = tablero.shape[1]     # calcula el número de columnas
@@ -40,16 +41,19 @@ def coloca_barco_plus(tablero, barco):
         columna = pieza[1]
 
         if fila < 0  or fila >= num_max_filas:
-            print(f"No puedo poner la pieza {pieza} porque se sale del tablero.")
-            return False
+            if mostrar_proceso:
+                print(f"No puedo poner la pieza {pieza} porque se sale del tablero.")
+                return False
         
         if columna < 0 or columna >= num_max_columnas:
-            print(f"No puedo poner la pieza {pieza} porque se sale del tablero.")
-            return False
+            if mostrar_proceso:
+                print(f"No puedo poner la pieza {pieza} porque se sale del tablero.")
+                return False
         
         if tablero[pieza] == "O" or tablero[pieza] == "X":
-            print(f"No puedo poner la pieza {pieza} porque hay otro barco.")
-            return False
+            if mostrar_proceso:
+                print(f"No puedo poner la pieza {pieza} porque hay otro barco.")
+                return False
         
         tablero_temp[pieza] = "O"
 
@@ -59,18 +63,20 @@ def coloca_barco_plus(tablero, barco):
 
 # PASO 4: funcion para colocar los barcos aleatoriamente
 
-def crea_barco_aleatorio(tablero,eslora = 4, num_intentos = 100):
+def crea_barco_aleatorio(tablero,eslora = 4, num_intentos = 100, mostrar_proceso = True):
     num_max_filas = tablero.shape[0]
     num_max_columnas = tablero.shape[1]
-    
+
     while True:
         barco = []          # Construimos el hipotetico barco
-        pieza_original = (random.randint(0,num_max_filas-1), random.randint(0, num_max_columnas -1))
-        print("Pieza original:", pieza_original)
+        pieza_original = (random.randint(0,num_max_filas - 1), random.randint(0, num_max_columnas - 1))
+        if mostrar_proceso:
+            print("Pieza original:", pieza_original)
         
         barco.append(pieza_original)
         orientacion = random.choice(["N","S","O","E"])
-        print("Con orientacion", orientacion, "\n")
+        if mostrar_proceso:
+            print("Con orientacion", orientacion, "\n")
         
         fila = pieza_original[0]
         columna = pieza_original[1]
@@ -87,12 +93,13 @@ def crea_barco_aleatorio(tablero,eslora = 4, num_intentos = 100):
             pieza = (fila,columna)
             barco.append(pieza)
         
-        tablero_temp = coloca_barco_plus(tablero, barco)
+        tablero_temp = coloca_barco_plus(tablero, barco, mostrar_proceso)
         
         if type(tablero_temp) == np.ndarray:
             return tablero_temp
         
-        print("Tengo que intentar colocar otro barco.\n")
+        if mostrar_proceso:
+            print("Tengo que intentar colocar otro barco.\n")
 
 
 
@@ -170,7 +177,7 @@ REGLAS DEL JUEGO
                 """)
             
         elif menu == 2:
-            print("¡Izad las velas mis valientes marineros!")
+            print("\n¡Izad las velas mis valientes marineros!")
             salir = True
         
         elif menu == 3:
@@ -201,7 +208,7 @@ REGLAS DEL JUEGO
     except ValueError:
         print("Por favor, introduce un número válido.")
         continue
-   
+
 
 
 # PASO 8: crear los jugadores, primero el contrincante y después nosotros
@@ -234,15 +241,15 @@ print(f"¡Que empiece la ballata {jugador}!")
 
 tamano = 0
 
-while tamano < 5:
-    tamano = int(input("Introduce los lados del tablero: "))
+while tamano < 8:
+    tamano = int(input("\nIntroduce los lados del tablero: "))
 
-    if tamano >= 5:
+    if tamano >= 8:
         tablero = crea_tablero(tamano)
         print(f"\n=== HUNDIR LA FLOTA ===\nTablero de juego: {tamano}x{tamano} \n")
         
     else:
-        print(f"Tablero demasiado pequeño. Crea otro mayor de {tamano} casillas, por favor.")
+        print(f"Tablero demasiado pequeño. Crea con más de {tamano} casillas, por favor.\n¡Al menos 8!")
 
 tablero_jugador = tablero       # ambos tableros miden lo mismo
 tablero_contrincante = tablero
@@ -253,17 +260,17 @@ while True:
     ver_tablero_contrincante = ver_tablero_contrincante.lower()
 
     if ver_tablero_contrincante == "si":
-        print(f"Sí necesito ayuda contra {contrincante}.\n")
+        print(f"Sí, necesito ayuda contra {contrincante}.\n")
         ver_tablero_contrincante = True
         break
     
     elif ver_tablero_contrincante == "no":
-        print(f"No pienso hacer trampas con {contrincante}.\n")
+        print(f"No, no pienso hacer trampas contra {contrincante}.\n")
         ver_tablero_contrincante = False
         break
     
     else:
-        print(f"Por favor {jugador}, responde si o no.")
+        print(f"Por favor {jugador}, responde si o no.\n")
 
 
 print(f"\nTablero de {jugador}\n\n{tablero_jugador}\n\n")
@@ -274,23 +281,28 @@ if ver_tablero_contrincante == True:
 
 # PASO 10: colocar los buques
 # coloco 6 barcos en total aleatoriamente
-
-tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 2))   # 3 barcos de 2m eslora
-tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 2))
-tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 2))
-tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 3))   # 2 naves de 3m eslora
-tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 3))
-tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 4))   # 1 buque de 4m de eslora
+print(f"\nColoquemos lo buques de {jugador}")
+print(f"_______________________________________\n")
+tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 2, mostrar_proceso = True))   # 3 barcos de 2m eslora
+tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 2, mostrar_proceso = True))
+tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 2, mostrar_proceso = True))
+tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 3, mostrar_proceso = True))   # 2 naves de 3m eslora
+tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 3, mostrar_proceso = True))
+tablero_jugador = (crea_barco_aleatorio(tablero_jugador, eslora = 4, mostrar_proceso = True))   # 1 buque de 4m de eslora
 print(f"\nTablero de {jugador}\n\n{tablero_jugador}\n\n")
 
-tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 2))   # 3 barcos de 2m eslora
-tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 2))
-tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 2))
-tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 3))   # 2 naves de 3m eslora
-tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 3))
-tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 4))   # 1 buque de 4m de eslora
+print(f"\nColoquemos lo buques de {contrincante}")
+print(f"_______________________________________\n")
+tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 2, mostrar_proceso = ver_tablero_contrincante))   # 3 barcos de 2m eslora
+tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 2, mostrar_proceso = ver_tablero_contrincante))
+tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 2, mostrar_proceso = ver_tablero_contrincante))
+tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 3, mostrar_proceso = ver_tablero_contrincante))   # 2 naves de 3m eslora
+tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 3, mostrar_proceso = ver_tablero_contrincante))
+tablero_contrincante = (crea_barco_aleatorio(tablero_contrincante, eslora = 4, mostrar_proceso = ver_tablero_contrincante))   # 1 buque de 4m de eslora
 if ver_tablero_contrincante == True:
-    print(f"\nTablero de {contrincante}\n\n{tablero_contrincante}\n\n") 
+    print(f"\nTablero de {contrincante}\n\n{tablero_contrincante}\n\n")
+else:
+    print(f"\nTablero de {contrincante} preparado.")
 
 
 
@@ -306,13 +318,17 @@ while True:
 
         if turno == 1:
             turno = True
-            print(f"\n{jugador} comenzará la partida.")
+            print(f"\n{jugador} comenzará la partida.\n")
             break
 
         elif turno == 2:
             turno = False
-            print(f"\n{contrincante} comenzará la partida.")
+            print(f"\n{contrincante} comenzará la partida.\n")
             break
+
+        else:
+            print("Escribe un número válido, ¿o es que llevas un garfio?")
+            continue
 
     except ValueError:
         print("Escribe un número válido, ¿o es que llevas un garfio?")
@@ -320,13 +336,27 @@ while True:
 
 
 
-# PASO 12: ataque
-
+# PASO 12: 
 # preguntar al tablero si tiene O (barcos) en alguna posición
 # retorna True cuando encuentre un barco y False cuando no haya o esté hundido
 # print(tablero_jugador == "O") <--- si quiero imprimir el tablero indicando los barcos
 
-while not False in tablero_jugador or not False in tablero_contrincante:
+game_over_jugador = not np.any(tablero_jugador == "O")
+game_over_contrincante = not np.any(tablero_jugador == "O")
+
+
+
+# PASO 13: ataque
+
+while True:
+
+    if not np.any(tablero_jugador == "O"):          # veo si me quedan barcos para seguir jugando
+        print(f"GAME OVER\n{jugador} se perdió en el Pacífico.")
+        break
+
+    if not np.any(tablero_contrincante == "O"):     # veo si al contrincante le quedan barcos para seguir jugando
+        print(f"YOU WIN\n{contrincante} naufragó en una isla desierta.")
+        break
 
     # si es el turno del jugador (turno = True)
     if turno:
@@ -340,21 +370,27 @@ while not False in tablero_jugador or not False in tablero_contrincante:
             columna = int(input("Columna: "))
         
         except ValueError:
-            print(f"Escribe sólo números del 1 al {tamano}")
+            print(f"Escribe sólo números del 1 al {tamano}\n")
             continue
             
         if not (1 <= fila <= tamano) or not (1 <= columna <= tamano):
             print(f"({fila},{columna})")
-            print("Escribe un número válido, ¿o es que llevas un garfio?\n")
+            print("\nEscribe un número válido, ¿o es que llevas un garfio?\n")
             continue
 
-        if (0 < fila <= tamano) and (0 < columna <= tamano):
-            print(f"({fila},{columna})")
-            disparar(tablero_contrincante, (fila -1, columna -1))
-            turno = False       # fin del turno del jugador
+        print(f"({fila},{columna})")
+        disparar(tablero_contrincante, (fila -1, columna -1))
+        turno = False       # fin del turno del jugador
             
-            print(f"\nTablero de {contrincante}\n{tablero_contrincante}\n\n")       
-            break    
+        print(f"\nTablero de {contrincante}\n{tablero_contrincante}\n\n")       
+        break 
+
+    # veo otra vez si al contrincante le quedan barcos para seguir jugando
+    # si no lo hago le estaría dando un turno más teniendo todos los barcos hundidos!!
+
+    if not np.any(tablero_contrincante == "O"):          
+        print(f"YOU WIN\n{contrincante} naufragó en una isla desierta.")
+        break  
 
     # si es el turno del contrincante (turno = False)
     if not turno:
@@ -368,12 +404,11 @@ while not False in tablero_jugador or not False in tablero_contrincante:
         turno = True        # fin del turno del contrincante
         
         print(f"\nTablero de {jugador}\n{tablero_jugador}\n\n")
+    
+    # veo otra vez si me quedan barcos para seguir jugando
+    # si no lo hago tendría un turno más teniendo todos mis barcos hundidos!!
 
-if False in tablero_jugador:        # si todas las casillas son falsas, es que no te quedan barcos
-    print(f"GAME OVER\n{jugador} se perdió en el Pacífico.")
+    if not np.any(tablero_jugador == "O"):
+        print(f"GAME OVER\n{jugador} se perdió en el Pacífico.")
+        break
 
-elif False in tablero_contrincante:
-    print(f"YOU WIN\n{contrincante} naufragó en una isla desierta.")
-
-else:
-    print("SIGUES VIVO")
